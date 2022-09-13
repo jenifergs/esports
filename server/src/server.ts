b27ca1b35d637495de70ad76d4b97c56614305ca
jenifergs <contato.comercialjenifer@gmail.com>
2022-09-12 22:02:12 -0300

    feat: :tada: Ambiente preparado para servidor, web e mobile.

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/ads", (req, res) => {
+    return res.json([
+        {id: 1 , title: "Ad 1"},
+        {id: 2 , title: "Ad 2"},
+        {id: 3 , title: "Ad 3"},
+        {id: 4 , title: "Ad 4"},
+
+    ]);
+});
+
+
+app.listen(3000);
